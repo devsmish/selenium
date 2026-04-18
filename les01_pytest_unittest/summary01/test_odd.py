074cfa2b3638e45d68f6ed61226c8ac070a181de
devsmish <smyshynskyi@gmail.com>
2026-04-18 12:36:08 +0200

    update test_odd.py

--- les01_pytest_unittest/summary01/test_odd.py
+++ les01_pytest_unittest/summary01/test_odd.py
@@ -8,9 +8,9 @@ def odd_checker():
 def test_odd(odd_checker):
     assert odd_checker.is_odd(2) == False
     assert odd_checker.is_odd(3) == True
-    assert odd_checker.is_odd(-2) == False
+    assert odd_checker.is_odd(-2) is False
 
 def test_even(odd_checker):
-    assert odd_checker.is_even(2) == True
+    assert odd_checker.is_even(2) is True
     assert odd_checker.is_even(3) == False
     assert odd_checker.is_even(-2) == True
