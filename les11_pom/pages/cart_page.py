2eb513940e2f81982bb6c29a2e8ae0b7d5f4b9c6
devsmish <smyshynskyi@gmail.com>
2026-05-20 20:18:40 +0200

    correct cart_page.py, inventory_page.py, login_page.py

--- les11_pom/pages/cart_page.py
+++ les11_pom/pages/cart_page.py
@@ -3,17 +3,17 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class CartPage:
-   def __init__(self, driver):
+    def __init__(self, driver):
        self.driver = driver
        self.wait = WebDriverWait(driver, 10)
 
-   def get_cart_item_price(self, item_name):
+    def get_cart_item_price(self, item_name):
        item_xpath = f"//div[text()='{item_name}']/ancestor::div[@class='cart_item']//div[@class='inventory_item_price']"
        return self.wait.until(EC.presence_of_element_located((By.XPATH, item_xpath))).text
 
-   def remove_item_from_cart(self, item_name):
+    def remove_item_from_cart(self, item_name):
        button_xpath = f"//div[text()='{item_name}']/ancestor::div[@class='cart_item']//button"
        self.wait.until(EC.element_to_be_clickable((By.XPATH, button_xpath))).click()
 
-   def proceed_to_checkout(self):
+    def proceed_to_checkout(self):
        self.wait.until(EC.element_to_be_clickable((By.ID, "checkout"))).click()
