88802935cc4f06e6310258b1575432ee693472cc
devsmish <smyshynskyi@gmail.com>
2026-05-20 15:10:29 +0200

    add hw6 test purchase_3_products

--- les11_pom/pages/checkout_step_two_page.py
+++ les11_pom/pages/checkout_step_two_page.py
@@ -9,4 +9,19 @@ class CheckoutStepTwoPage:
 
     def click_finish(self):
        self.wait.until(EC.element_to_be_clickable((By.ID, "finish"))).click()
-       
+
+    def get_total_price(self):
+        """
+        Находит элемент общей стоимости, очищает текст от 'Total: $'
+        и возвращает чистую строку с ценой (например, '58.29')
+        """
+        raw_text = self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.summary_total_label'))).text
+        cleaned_price = raw_text.replace("Total: $", "").strip()
+        return cleaned_price
+
+    def verify_total_price(self, expected_price):
+        """Метод проверяет, что итоговая цена на странице совпадает с ожидаемой."""
+        actual_price = self.get_total_price()
+
+        assert actual_price == str(expected_price), \
+            f"Ошибка! Ожидали цену '{expected_price}', но на странице отображается '{actual_price}'"
--- les11_pom/tests/constants.py
+++ les11_pom/tests/constants.py
@@ -4,3 +4,4 @@ PRODUCT_NAME="Sauce Labs Backpack"
 FIRST_NAME='Serg'
 LAST_NAME='Tester'
 POSTAL_CODE='77777'
+PRODUCTS_LIST=["Sauce Labs Backpack", "Sauce Labs Bolt T-Shirt", "Sauce Labs Onesie"]
